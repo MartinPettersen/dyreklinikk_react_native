e22f824c94e2c0ed60424e722e810a86b586bbf5
MartinPettersen <searchingtheskyfor@gmail.com>
2024-08-09 10:47:40 +0200

    add display a clinics employees

--- components/(universal)/ClinicPage.tsx
+++ components/(universal)/ClinicPage.tsx
@@ -1,42 +1,103 @@
-import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
-import { Clinic } from '../../utils/types'
+import React, { useEffect, useState } from "react";
+import { Text, View, StyleSheet, SafeAreaView, FlatList } from "react-native";
+import { Clinic } from "../../utils/types";
+import { FIRESTORE_DB } from "../../firebaseConfig";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import EmployeeTag from "../(util)/EmployeeTag";
 
 type Props = {
-    clinic: Clinic,
-    navigation: any,
-}
+  clinic: Clinic;
+  navigation: any;
+};
 
-const ClinicPage = ({clinic, navigation}: Props) => {
+type RenderEmployeeProp = {
+  item: any;
+};
+
+const ClinicPage = ({ clinic, navigation }: Props) => {
+  const [employees, setEmployees] = useState<any[] | null>(null);
+
+  const getEmployees = () => {
+    const employeeRef = collection(FIRESTORE_DB, "employees");
+
+
+    const subscriber = onSnapshot(
+      query(employeeRef, where("workplace", "==", clinic.id)),
+      {
+        next: (snapshot) => {
+          const employeeList: any[] = [];
+          snapshot.docs.forEach((doc) => {
+            employeeList.push({
+              name: doc.data().name,
+              expertise: doc.data().expertise,
+              patients: doc.data().patients,
+              id: doc.data().id,
+            });
+          });
+          console.log(employeeList)
+          setEmployees(employeeList);
+        },
+      }
+    );
+
+  };
+
+  useEffect(() => {
+    getEmployees();
+  }, []);
+
+  const renderEmployee = ({item}: RenderEmployeeProp) => {
+    return <EmployeeTag employee={item} action={() => console.log("h")} />
+  }
+
   return (
-    <View style={styles.container}>
-        <Text style={styles.headline}>{`${clinic.name}`}</Text>
-        <Text style={styles.text}><Text style={styles.bold}>Adresse: </Text>{clinic.adress}</Text>
-        <Text style={styles.text}><Text style={styles.bold}>Åpningstider: </Text>{`${clinic.openingHour}-${clinic.closingHour}`}</Text>
-        <Text style={styles.text}><Text style={styles.bold}></Text></Text>
-        <Text style={styles.text}><Text style={styles.bold}></Text></Text>
-        <Text style={styles.text}><Text style={styles.bold}></Text></Text>
-
-    </View>
-  )
-}
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.headline}>{`${clinic.name}`}</Text>
+      <Text style={styles.text}>
+        <Text style={styles.bold}>Åpningstider: </Text>
+        {`${clinic.openingHour}-${clinic.closingHour}`}
+      </Text>
+      <Text style={styles.text}>
+        <Text style={styles.bold}>Adresse: </Text>
+        {clinic.adress}
+      </Text>
+      <Text style={styles.text}>
+        <Text style={styles.bold}></Text>
+      </Text>
+      <Text style={styles.text}>
+        <Text style={styles.bold}>Våre Ansatte:</Text>
+      </Text>
+      {employees ? (
+      <View style={styles.employeesContainer}>
+        {
+          <FlatList
+            data={employees}
+            renderItem={renderEmployee}
+            keyExtractor={(employee: any) => employee.id}
+          />
+        }
+      </View>
+    ) : null}
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    headline: {
-      fontSize: 40,
-      margin: 20,
-      color: "#52525b",
-      fontWeight: "bold",
-    },
-    buttonContainer: {
-      margin: 10,
-      width: "50%",
-    },
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  headline: {
+    fontSize: 40,
+    margin: 20,
+    color: "#52525b",
+    fontWeight: "bold",
+  },
+  buttonContainer: {
+    margin: 10,
+    width: "50%",
+  },
   text: {
     fontSize: 20,
     color: "#52525b",
@@ -45,6 +106,9 @@ const styles = StyleSheet.create({
   bold: {
     fontWeight: "bold",
   },
-  });
+  employeesContainer: {
+    margin: 0,
+  },
+});
 
-export default ClinicPage
+export default ClinicPage;
